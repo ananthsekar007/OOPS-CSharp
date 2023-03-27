b018ebe67331262b53eda1b59829487e27a8b176
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-27 11:03:39 +0530

    Added a food class

---
+++ OOPS/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Food
+    {
+        static Dictionary<string, int> foods = new()
+        {
+            ["banana"] = 100,
+            ["egg"] = 40,
+        };
+
+        public static int GetCalorie(string foodName)
+        {
+            return foods[foodName];
+        }
+    }
+}
