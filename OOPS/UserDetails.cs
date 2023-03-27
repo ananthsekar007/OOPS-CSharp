43b11f303384873d84c1b2e3ccbcb34ac0d18864
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-27 11:03:28 +0530

    Added a user details class

---
+++ OOPS/UserDetails.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class UserDetails
+    {
+        string name;
+        int age;
+        float height;
+        float weight;
+        int calorieLimit;
+
+        public UserDetails(string name,  int age, float height, float weight)
+        {
+            this.name = name;    
+            this.age = age;
+            this.height = height;
+            this.weight = weight;
+            this.CalculateCalorieLimit();
+        }
+
+        private void CalculateCalorieLimit()
+        {
+            this.CalorieLimit = Convert.ToInt32(66.5f + 13.8 * this.weight + 5 * this.height / (6.8f * this.age));
+        }
+
+        public int CalorieLimit
+        {
+            get { return this.calorieLimit;}
+            set { this.calorieLimit = value; }
+        }
+
+        public void EatFood(string foodName)
+        {
+            this.calorieLimit -= Food.GetCalorie(foodName);
+        }
+    }
+}
