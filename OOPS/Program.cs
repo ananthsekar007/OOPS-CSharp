1788fa534926da3f67aa535785ac2559579b5fce
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-27 11:03:57 +0530

    Added basic calorie tracking application

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the User Details \n \n");
+            Console.Write("Enter Name : ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Height : ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Weight : ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            UserDetails userDetails = new UserDetails(name, age, height, weight);
+            
+            while(true)
+            {
+            Console.WriteLine("Start Eating food, once you want to stop , enter \"stop\" or else enter \"continue\" ");
+                if(Console.ReadLine() == "stop")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("What are you eating? : ");
+                    userDetails.EatFood(Console.ReadLine());
+                }
+            }
+            Console.WriteLine($" \n\n The remaining calories for the day is {userDetails.CalorieLimit} ");
         }
     }
 }
