87b61cd454a58af557a5b490f033d07760fad4d5
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-28 14:04:21 +0530

    Added orderby functionality

--- OOPS/Lambda.cs
+++ OOPS/Lambda.cs
@@ -18,6 +18,30 @@ namespace OOPS
             List<int> list = new() { 1,5,10,12,15,18,20 };
             List<int> divisibleBy5 = list.FindAll(x => x % 5 == 0);
             foreach (int x in divisibleBy5) {  Console.WriteLine(x); }
+
+            List<string> strList = new() { "ananth", "sekar", "sundar", "dumb" };
+            List<string> filteredList = strList.FindAll(x => x.Contains("d"));
+            foreach (string str in filteredList)
+            {
+                Console.WriteLine(str);
+            }
+            List<Student> studentList = new() 
+            { 
+                new Student() { Age = 20, Dept = "CSE", Name= "Ananth"  },
+                new Student() { Age = 17, Dept = "CSE", Name = "Ananth1" } 
+            };
+
+            foreach(Student student in studentList.OrderBy(student => student.Age > 18).ToList()) {
+            
+                Console.WriteLine(student.Name);
+            }
+
         }
+    }
+    internal class Student
+    {
+        public int Age { get; set;}
+        public string Name { get; set;}
+        public string Dept { get; set;}
     }
 }
