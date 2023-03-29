d8db230af1d2ca11f6b1264a129a5e329b95af17
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-29 09:52:04 +0530

    Added sample builtin delegate example

--- OOPS/Delegate.cs
+++ OOPS/Delegate.cs
@@ -9,17 +9,29 @@ namespace OOPS
     delegate void AddDelegate(int a, int b);
     internal class Delegates
     {
+        delegate void PrintStuff();
          void Main(string[] args)
         {
-            AddDelegate addDelegate = Add;
-            addDelegate += Subtract;
-            addDelegate(2,4);
-            addDelegate(3,4);
-            Delegate[] list = addDelegate.GetInvocationList();
-            foreach (Delegate d in list)
-            {
-                Console.WriteLine(d.DynamicInvoke(1, 1));
-            }
+            //adddelegate adddelegate = add;
+            //adddelegate += subtract;
+            //adddelegate(2,4);
+            //adddelegate(3,4);
+            //delegate[] list = adddelegate.getinvocationlist();
+            //foreach (delegate d in list)
+            //{
+            //    console.writeline(d.dynamicinvoke(1, 1));
+            //}
+
+            PrintStuff printStuff = HelloWorld;
+            printStuff.Invoke();
+
+            Action<string> action = (name) => {Console.WriteLine(name);};
+            action.Invoke("Ananth");
+        }
+
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
         }
 
         static void Add(int a, int b)
