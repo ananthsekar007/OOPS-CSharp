721695055c0be71fc28710498344058d33a8df18
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-29 09:52:19 +0530

    Added a class for testing builtin delegates

---
+++ OOPS/BuiltinDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class BuiltinDelegate
+    {
+
+        static void Main(string[] args) { 
+
+            //Action Built in type
+            Action<string> action = (name) => Console.WriteLine($" Hello, {name}");
+            action.Invoke("Ananth");
+
+            //Func Built in type
+            Func<int, int,int,  string> func = (a, b, c) => $" The Sum = {a + b + c} ";
+            Console.WriteLine(func.Invoke(1,2,3));
+                
+            //Predicate Built in type
+            Predicate<string> predicate = (a) => a.Contains("a") ;
+            Console.WriteLine(predicate.Invoke("dumb"));
+
+        }
+    }
+}
